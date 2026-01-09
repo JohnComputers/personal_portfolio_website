e7d06afbf80d6c76495d8f631924cea6474ded90
JohnComputers <johncomputers2024@gmail.com>
2026-01-09 11:15:22 -0500

    Add main components to App component

--- app/App.jsx
+++ app/App.jsx
@@ -1,1 +1,20 @@
+import Hero from "./components/Hero"
+import Capabilities from "./components/Capabilities"
+import Projects from "./components/Projects"
+import TechStack from "./components/TechStack"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
+export default function App() {
+  return (
+    <div className="bg-black text-white overflow-x-hidden">
+      <Hero />
+      <Capabilities />
+      <Projects />
+      <TechStack />
+      <About />
+      <Contact />
+    </div>
+  )
+}
 
